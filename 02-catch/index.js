1091652a39ffbd0c9dc4beaea79b21a0b413eeb8
Annie Huang <annie.hn@gmail.com>
2019-01-19 22:06:18 +1100

    Lesson 2 notes update

--- 02-catch/index.js
+++ 02-catch/index.js
@@ -14,6 +14,7 @@ output.innerText = "Loading ...";
 // fetch(API_URL + "films")
 fetch(API_URL + "movies")
   .then(response => {
+    // Handle failed network request. e.g. None success http status code.
     if (!response.ok) {
       throw Error("Unsuccessful response");
     }
@@ -25,3 +26,28 @@ fetch(API_URL + "movies")
     console.warn(error);
     output.innerText = ":(";
   });
+
+/*
+
+// This below is different from above.
+// Just do response and error does not handle response.json() returns with a error code.
+// It only handles if the response is rejected by the network.
+fetch(API_URL + "movies")
+  .then(
+    // onFulfilled. Only onFulfilled or onRejected will be call, NEVER BOTH.
+    response => {
+      console.log(response);
+      // e.g.
+      // return Promise.reject("Invalid JSON").then(films => {
+      return response.json().then(films => {
+        output.innerText = getFilmTitles(films);
+      }, error => {console.warn(error);});
+    },
+    // onRejected
+    error => {
+      console.warn(error);
+      output.innerText = ":(";
+    }
+  );
+
+*/
