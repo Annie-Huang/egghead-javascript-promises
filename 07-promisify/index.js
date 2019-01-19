c42d50b0f7783c2289b9b077a2b637236f12862e
Annie Huang <annie.hn@gmail.com>
2019-01-20 10:20:06 +1100

    Lesson 7 notes update

--- 07-promisify/index.js
+++ 07-promisify/index.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 const util = require("util");
 
+// It is working in node.js environment.
+// If you don't have access to the util module, because you're working in another environment such as the browser,
+// you can either go with a manual approach that we've seen before,
+// or you can find yourself a package that implements a generic promisify method.
+// As we've seen, converting callback style functions to promise based ones is mostly boilerplate.
+// You don't want to be repeating yourself over and over again.
 const readFile = util.promisify(fs.readFile);
 
 readFile(__filename, "utf8").then(
