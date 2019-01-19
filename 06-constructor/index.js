6dab3b60299c4aa7d711438938c2f8fe0bc8d5dc
Annie Huang <annie.hn@gmail.com>
2019-01-20 10:11:28 +1100

    Lesson 6 notes update

--- 06-constructor/index.js
+++ 06-constructor/index.js
@@ -1,8 +1,16 @@
 function sleep(ms) {
   return new Promise(resolve => {
+    // throw new Error("...");
     setTimeout(resolve, ms);
   });
 }
+
+// // The above is a simplified version the following..
+// function sleep(ms) {
+//   return new Promise(resolve => {
+//     setTimeout(() => {resolve()}, ms);
+//   });
+// }
 
 console.log("Right away");
 
