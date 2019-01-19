0c01a462550e8e5f338b00a375e37f2e28712ab1
Annie Huang <annie.hn@gmail.com>
2019-01-20 09:56:59 +1100

    Lesson 5 notes udpate

--- 05-resolve/index.js
+++ 05-resolve/index.js
@@ -14,6 +14,11 @@ function getFilmTitles(films) {
     .join("\n");
 }
 
+// jQuery was written long before we had native promises in JavaScript.
+// jQuery ships with its own promise implementation. That implementation doesn't have a method called "finally."
+// We can fix this by using Promise.resolve. This way, the non-standard promise returned by the getJSON method
+// will be converted to a proper native promise. If we refresh now, we see that our page is working again.
+// This works for all promise-like objects with a then method. These objects are also known as thenables.
 Promise.resolve($.ajax(API_URL + "films"))
   .then(films => {
     output.innerText = getFilmTitles(films);
