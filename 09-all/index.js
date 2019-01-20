517618c200bc622ccfc991c0364f5e2cf4fc221c
Annie Huang <annie.hn@gmail.com>
2019-01-20 11:49:25 +1100

    Lesson 9 notes update

--- 09-all/index.js
+++ 09-all/index.js
@@ -11,6 +11,18 @@ function queryAPI(endpoint) {
   });
 }
 
+// // If you do it in sequential way, the 2nd promise will only be fired out after return from the 1st promise.
+// queryAPI("films").then(films => {
+//   return queryAPI("planets").then(planets => {
+//     output.innerText =
+//       '${films.length} films, ' +
+//       '${planets.length} planets, ' ;
+//   }) ;
+// })
+//   .finally(() => {
+//     spinner.remove();
+//   });
+
 Promise.all([
   queryAPI("films"),
   queryAPI("planets"),
@@ -29,3 +41,22 @@ Promise.all([
   .finally(() => {
     spinner.remove();
   });
+
+
+// // The order of the return response will be the same as the request.
+// const promise = Promise.all([
+//   queryAPI("films"),
+//   queryAPI("planets"),
+// ]);
+//
+// promise
+//   .then(results => {
+//     const films = results[0];
+//     const planets = results[1];
+//     output.innerText =
+//       '${films.length} films, ' +
+//       '${planets.length} planets ';
+//   })
+//   .finally(() => {
+//     spinner.remove();
+//   });
